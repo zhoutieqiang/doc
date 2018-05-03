e89a7207b00846cbbdf32b284036e1d2c7044b8d
zhoutieqiang <zhoutieqiang@163.com>
2018-05-03 15:49:14 +0800

    从老虎获取股票历史数据

--- js/extend.js
+++ js/extend.js
@@ -1,4 +1,4 @@
-﻿		String.prototype.trim=function(){
+﻿String.prototype.trim=function(){
 　　    return this.replace(/(^\s*)|(\s*$)/g, "");
 　　 }
 　　 String.prototype.ltrim=function(){
@@ -8,7 +8,7 @@
 　　    return this.replace(/(\s*$)/g,"");
 　　 }
 
- function trim(str){ //删除左右两端的空格
+function trim(str){ //删除左右两端的空格
 　　     return str.replace(/(^\s*)|(\s*$)/g, "");
 　　 }
 　　 function ltrim(str){ //删除左边的空格
@@ -18,11 +18,35 @@
 　　     return str.replace(/(\s*$)/g,"");
 　　 }
 
+
 (function () {
     var environment = "DEV";//PRD、DEV、STG1、STG2
     
     window.phutil = {
 
+
+		dateformat:function(date, fmt)   
+		{ 
+		//author:wangweizhen
+		//用法示例：
+		//var date = new Date(1403149534);
+		//phutil.dateformat(date, "yyyy-MM-dd hh:mm:ss");
+		  var o = {   
+			"M+" : date.getMonth()+1,                 //月份   
+			"d+" : date.getDate(),                    //日
+			"h+" : date.getHours(),                   //小时   
+			"m+" : date.getMinutes(),                 //分   
+			"s+" : date.getSeconds(),                 //秒   
+			"q+" : Math.floor((date.getMonth()+3)/3), //季度   
+			"S"  : date.getMilliseconds()             //毫秒   
+		  };   
+		  if(/(y+)/.test(fmt))   
+			fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));   
+		  for(var k in o)   
+			if(new RegExp("("+ k +")").test(fmt))   
+		  fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
+		  return fmt;   
+		},
         /**
          * 向本地保存对象数据
          */
